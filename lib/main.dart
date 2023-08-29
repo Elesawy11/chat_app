165dda8f7d24b44155b6c19338bea08a75f8e3a0
elesawy <elesawy928@gmail.com>
2023-08-29 20:34:05 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  return runApp(const ChatApp());
+}
+
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
